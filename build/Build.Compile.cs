c1adb34865eae47e4fc80e4a7cbb7face49e2fa5
Nice3point <nice3point@gmail.com>
2025-01-10 01:16:52 +0300

    Add Compile target

---
+++ build/Build.Compile.cs
@@ -0,0 +1,17 @@
+﻿using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+sealed partial class Build
+{
+    Target Compile => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            ValidateRelease();
+
+            DotNetBuild(settings => settings
+                .SetConfiguration("Release")
+                .SetOutputDirectory(ArtifactsDirectory)
+                .SetVerbosity(DotNetVerbosity.minimal));
+        });
+}
--- build/Build.cs
+++ build/Build.cs
@@ -6,5 +6,5 @@ sealed partial class Build : NukeBuild
     [GitRepository] readonly GitRepository GitRepository;
     [Solution(GenerateProjects = true)] readonly Solution Solution;
 
-    public static int Main() => Execute<Build>(build => build.Pack);
+    public static int Main() => Execute<Build>(build => build.Compile);
 }
